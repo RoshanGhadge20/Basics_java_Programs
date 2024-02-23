5ffb510f0f3449bc12206ef10dbbae081bdcff18
Basics_java_Programs <roshanghadge20@gmail.com>
2024-02-23 16:35:57 +0530

    first initial commit

---
+++ src/Prog_Code/First_Basic_Program.java
@@ -0,0 +1,8 @@
+package Prog_Code;
+
+public class First_Basic_Program {
+	public static void main(String[] args) {
+		System.out.println("THIS IS THE FIRST JAVA PROGRAM EXECUTED");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Basic_Java_Programs {
+}
