ffa09db2ddd715f6196fa5fce1dee93e296c26f9
Basic_java_Programs <roshanghadge20@gmail.com>
2024-02-23 17:30:03 +0530

    this is the second commit

--- src/Prog_Code/First_Basic_Program.java
+++ src/Prog_Code/First_Basic_Program.java
@@ -3,6 +3,7 @@ package Prog_Code;
 public class First_Basic_Program {
 	public static void main(String[] args) {
 		System.out.println("THIS IS THE FIRST JAVA PROGRAM EXECUTED");
+		System.out.println("End of program execution ");
 	}
 
 }
