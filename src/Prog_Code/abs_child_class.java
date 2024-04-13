8496c9091d68e5a734bfd8690085e73c95ede300
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-04-13 17:15:19 +0530

    Commit made after practice of abstraction

---
+++ src/Prog_Code/abs_child_class.java
@@ -0,0 +1,22 @@
+package Prog_Code;
+
+public class abs_child_class extends abstraction_practice{
+
+	@Override
+	public void jet() {
+		System.out.println("calling u from jet method");
+		
+	}
+	
+	public static void main(String args[])
+	{
+		abs_child_class obj=new abs_child_class();
+		obj.get();
+		obj.set();
+		obj.met();
+		obj.jet();
+		System.out.println("End of program execution");
+	}
+	
+
+}
---
+++ src/Prog_Code/abstraction_practice.java
@@ -0,0 +1,23 @@
+package Prog_Code;
+
+public abstract class abstraction_practice {
+	
+	public void get()
+	{
+		System.out.println("Calling u from get method");
+	}
+	
+	public void set()
+	{
+		System.out.println("Calling u from set method");
+	}
+	
+	public void met()
+	{
+		System.out.println("Calling u from met method");
+	}
+	
+	public abstract void jet();
+	
+
+}
