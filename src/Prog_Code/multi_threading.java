0c726d42f9d5342aeeab91f89a81681e92f88773
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-07-01 18:03:20 +0530

    added few practice sets of program

---
+++ src/Prog_Code/multi_threading.java
@@ -0,0 +1,30 @@
+package Prog_Code;
+
+public class multi_threading extends Thread
+{
+	public void run()
+	{
+		
+		try 
+		{
+			System.out.println(" Thread "+ Thread.currentThread().getId()+" is running ");
+		} 
+		catch (Exception e) 
+		{
+			System.out.println("Exception caught");
+		}
+		
+	}
+	
+	public static void main(String[] args) 
+	{
+		int n=8;
+		for (int i=1;i<=n;i++)
+		{
+			Thread obj= new Thread(new multi_threading());
+			obj.start();
+		}
+		
+	}
+
+}
