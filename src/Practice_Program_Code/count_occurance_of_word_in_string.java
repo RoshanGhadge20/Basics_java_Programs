e0eb663631219f2fcdc3fc1042ad98f268ebffc6
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-08-07 17:41:26 +0530

    practice set with few program code

---
+++ src/Practice_Program_Code/count_occurance_of_word_in_string.java
@@ -0,0 +1,24 @@
+package Practice_Program_Code;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class count_occurance_of_word_in_string 
+{
+	public static void main(String[] args) 
+	{
+		
+		String nameString="Test java java test automation";
+		int count=0;
+		for(int i=0;i<nameString.length();i++)
+		{
+			if ((nameString.charAt(i)==''))
+			{
+				count++;
+			}
+		}
+		
+		System.out.println("Counting the occurance of word into string "+ countHashMap);
+		
+	}
+}
