cc8b741cfa159f10cc2d86d997c3b3a678b06f84
Roshan Ghadge <roshanghadge568@gmail.com>
2025-08-01 18:30:10 +0530

    Working with advance exceptions

---
+++ src/Custom_Exceptions/MyException.java
@@ -0,0 +1,11 @@
+package Custom_Exceptions;
+
+public class MyException extends Exception {
+
+    // Custom Exception class is created for checked exceptions
+
+    public MyException(String m) {
+        super(m);
+    }
+
+}
